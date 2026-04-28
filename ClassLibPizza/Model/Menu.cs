ac968416861614593aea5e09bb6c12ba615e29f3
Christian Stenholm Wendt-Jensen <chw003@edu.zealand.dk>
2026-04-28 10:50:29 +0200

    creaded Pizzza class + list in Menu with pizzas + methods

--- ClassLibPizza/Model/Menu.cs
+++ ClassLibPizza/Model/Menu.cs
@@ -4,7 +4,71 @@ using System.Text;
 
 namespace ClassLibPizza.Model
 {
-	internal class Menu
+	public class Menu
 	{
+		private List<Pizza> _pizzas;
+
+		public Menu()
+		{
+		_pizzas = new List<Pizza>();
+		}
+		public Menu(List<Pizza> pizzas) 
+		{
+		_pizzas = pizzas;
+		}
+
+		public List<Pizza> Pizzas
+		{
+			get { return _pizzas; }
+			set { _pizzas = value; }
+		}
+
+		#region Methods
+
+		public override string ToString()
+		{
+			string res = " ";
+			foreach (Pizza pizza in _pizzas) 
+			{
+				res = res + pizza + " Name= " + pizza.Name;
+			}
+			return res;
+		}
+
+
+
+		public void AddPizza(Pizza pizza)
+		{
+			_pizzas.Add(pizza);
+		}
+
+		public Pizza GetPizza(int id)
+		{
+			Pizza pizzaFound = null;
+			foreach (var pizza in _pizzas)
+			{
+				if (pizza.PizzaNo == id)
+				{
+					return pizza;
+				}
+			}
+			return pizzaFound;
+		}
+
+		public Pizza UpdatePizza (int id, Pizza updatedPizza)
+		{
+			Pizza PizzaToUpdate = GetPizza(id);
+			PizzaToUpdate.Name = updatedPizza.Name;
+			PizzaToUpdate.Price = updatedPizza.Price;
+			PizzaToUpdate.Vegan = updatedPizza.Vegan;
+			PizzaToUpdate.Calzone = updatedPizza.Calzone;
+			PizzaToUpdate.Gluten = updatedPizza.Gluten;
+			PizzaToUpdate.Ingrediens = updatedPizza.Ingrediens;
+
+			return PizzaToUpdate;
+		}
+
+
+		#endregion
 	}
 }
--- ClassLibPizza/Model/Pizza.cs
+++ ClassLibPizza/Model/Pizza.cs
@@ -8,20 +8,93 @@ namespace ClassLibPizza.Model
 	{
 
 		#region instance fields
-
+		private int _pizzaNo;
+		private string _name;
+		private double _price;
+		private bool _vegan;
+		private bool _calzone;
+		private bool _gluten;
+		private string _ingrediens;
+		private bool _delivery;
 
 		#endregion
 
 		#region constructors
+		public Pizza()
+		{
+			_pizzaNo = 0;
+			_name = "dummy";
+			_price = 0;
+			_vegan = false;
+			_calzone = false;
+			_gluten = false;
+			_ingrediens = "dummy";
+			_delivery = false;
+		}
 
+		public Pizza (int pizzaNo, string name, double price, bool vegan, bool calzone, bool gluten, string ingrediens, bool delivery)
+		{
+			_pizzaNo = pizzaNo;
+			_name = name;
+			_price = price;
+			_vegan = vegan;
+			_calzone = calzone;
+			_gluten = gluten;
+			_ingrediens = ingrediens;
+			_delivery = delivery;
+		}
 		#endregion
 
 		#region properties
+		public int PizzaNo
+		{
+			get { return _pizzaNo; }
+			set { _pizzaNo = value; }
+		}
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value; }
+		}
+		public double Price
+		{
+			get { return _price; }
+			set { _price = value; }
+		}
+		public bool Vegan
+		{
+			get { return _vegan; }
+			set { _vegan = value; }
+		}
+		public bool Calzone
+		{
+			get { return _calzone; }
+			set { _calzone = value; }
+		}
+		
+		public bool Gluten
+		{
+			get { return _gluten; }
+			set { _gluten = value; }
+		}
 
+		public string Ingrediens
+		{
+			get { return _ingrediens; }
+			set { _ingrediens = value; }
+		}
+		public bool Delivery
+		{
+			get { return _delivery; }
+			set { _delivery = value; }
+		}
 		#endregion
 
 		#region methods()
-
+		public override string ToString()
+		{
+			return "PizzaNo= " + PizzaNo + " Name= " + Name + " Price= " + Price + " Vegan= " + Vegan + " Calzone= " + Calzone + " Gluten= " + Gluten + " Ingridiens= " + Ingrediens + " Delivery= " + Delivery;
+		}
 		#endregion
 	}
 }
