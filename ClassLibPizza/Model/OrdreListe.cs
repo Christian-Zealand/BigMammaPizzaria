7ed5c65d96051fe931167060214beba435a75683
ThomasDalgaardHavaleska <thh009@edu.zealand.dk>
2026-04-28 13:15:29 +0200

    Made OrdreListe Class med Inst. Cons. Props og to string. Miss: Methoeds

--- ClassLibPizza/Model/OrdreListe.cs
+++ ClassLibPizza/Model/OrdreListe.cs
@@ -6,6 +6,41 @@ namespace ClassLibPizza.Model
 {
 	public class OrdreListe
 	{
+		//Instancefields
+		private List<Ordre> _listeAllOrdre;
+
+
+		//Constructor
+
+		public OrdreListe()
+		{
+			_listeAllOrdre = new List<Ordre>();
+		}	
+
+		public OrdreListe(List<Ordre> listeAllOrdre)
+		{
+			_listeAllOrdre = listeAllOrdre;
+		}
+
+		//Properties
+
+		public List<Ordre> ListeAllOrdre
+		{
+			get { return _listeAllOrdre; }
+			set { _listeAllOrdre = value; }
+		}
+
+		//ToString:
+
+		public override string ToString()
+		{
+			string result = "OrdreListe for all pizzas: \n";
+			foreach (Ordre ordre in _listeAllOrdre)
+			{
+				result = result + ordre;
+			}
+			return result;
+		}
 
 	}
 }
