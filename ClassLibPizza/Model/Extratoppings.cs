8d8e2e3e3cc773f61ed387268729fdf4c4778c77
ThomasDalgaardHavaleska <thh009@edu.zealand.dk>
2026-04-28 11:30:48 +0200

    Created Classes: Extratoppings + ExtratoppingsList

---
+++ ClassLibPizza/Model/Extratoppings.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ClassLibPizza.Model
+{
+	public class Extratoppings
+
+
+
+	{
+		//Instancefields
+		private int _extraToppingId;
+		private string _extraToppingName;
+		private double _extraToppingPrice;
+
+		//Constructor
+
+		public Extratoppings()
+		{
+			_extraToppingId = 0;
+			_extraToppingName = "";
+			_extraToppingPrice = 0.0;
+
+		}
+
+		public Extratoppings(int extraToppingId, string extraToppingName, double extraToppingPrice)
+		{
+			_extraToppingId = extraToppingId;
+			_extraToppingName = extraToppingName;
+			_extraToppingPrice = extraToppingPrice;
+
+		}
+		//Properties
+
+		public int ExtraToppingId
+		{
+			get { return _extraToppingId; }
+			set { _extraToppingId = value; }
+		}
+		public string ExtraToppingName
+		{
+			get { return _extraToppingName; }
+			set { _extraToppingName = value; }
+		}
+		public double ExtraToppingPrice
+		{
+			get { return _extraToppingPrice; }
+			set { _extraToppingPrice = value; }
+		}
+
+		//ToString:
+		public override string ToString()
+		{
+			return " Topping ID: " + ExtraToppingId + "Topping: " + ExtraToppingName + "Price: " + ExtraToppingPrice;
+		}
+
+
+
+
+	}
+}
+
---
+++ ClassLibPizza/Model/ExtratoppingsList.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibPizza.Model
+{
+	public class ExtratoppingsList
+
+
+	{
+		//Instancefields
+		private List<Extratoppings> _extratoppingsList;
+
+		//Constructor
+		public ExtratoppingsList()
+		{
+			_extratoppingsList = new List<Extratoppings>();
+		}
+
+		public ExtratoppingsList(List<Extratoppings> extratoppingsList)
+		{
+			_extratoppingsList = extratoppingsList;
+		}
+
+		//Properties
+		public List<Extratoppings> Extratoppings
+		{
+			get { return _extratoppingsList; }
+			set { _extratoppingsList = value; }
+		}
+
+		//ToString:
+
+		public override string ToString()
+		{
+			string result = "ExtratoppingsList:\n";
+
+			foreach (Extratoppings extratopping in _extratoppingsList)
+			{
+				result = result + extratopping;
+			}
+			return result;
+		}
+
+
+		//Methods Create, update, delete, read - CRUD
+		public void AddExtratopping(Extratoppings extratopping)
+		{
+			_extratoppingsList.Add(extratopping);
+		}
+
+		public List<Extratoppings> GetAllExtratoppings()
+		{
+			return new List<Extratoppings>(_extratoppingsList);
+		}
+
+		public Extratoppings Get(int extraToppingId)
+
+		{
+			Extratoppings extratoppingfound = null;
+			foreach (Extratoppings ToppingId in _extratoppingsList)
+			{
+				if (ToppingId.ExtraToppingId == extraToppingId)
+				{
+					extratoppingfound = ToppingId;
+
+				}
+			}
+			if (extratoppingfound != null)
+			{
+				return extratoppingfound;
+			}
+			else
+			{
+				throw new Exception("Extratopping with ID: " + extraToppingId + "not found.");
+			}
+		}
+
+		public Extratoppings Remove(int extraToppingId)
+		{
+			Extratoppings removeExtratopping = Get(extraToppingId);
+			_extratoppingsList.Remove(removeExtratopping);
+			return removeExtratopping;
+		}
+
+		public Extratoppings Update(int extraToppingId, Extratoppings updatedExtratopping)
+		{
+			Extratoppings updateExtratoppings = Get(extraToppingId);
+			updateExtratoppings.ExtraToppingName = updatedExtratopping.ExtraToppingName;
+			updateExtratoppings.ExtraToppingPrice = updatedExtratopping.ExtraToppingPrice;
+
+			return updateExtratoppings;
+
+		}
+
+	}
+}
+
