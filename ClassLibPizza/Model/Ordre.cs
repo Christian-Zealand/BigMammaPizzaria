77713c57b3788b1111ae8998a4ec61f9552bee93
Christian Stenholm Wendt-Jensen <chw003@edu.zealand.dk>
2026-04-28 12:32:42 +0200

    created Order class

--- ClassLibPizza/Model/Ordre.cs
+++ ClassLibPizza/Model/Ordre.cs
@@ -4,7 +4,48 @@ using System.Text;
 
 namespace ClassLibPizza.Model
 {
-	internal class Ordre
+	public class Ordre
 	{
+		private int _orderNo;
+		private int _date;
+		private double _tPrice;
+		private List<Pizza> _pizzaList;
+
+		public Ordre()
+		{
+		_orderNo = 0;
+		_date = 0;
+		_tPrice = 0;
+		_pizzaList = new List<Pizza>();
+		}
+		public Ordre (int orderNo, int date, double tPrice, List<Pizza> pizzaList)
+		{
+		_orderNo =orderNo;
+		_date=date;
+		_tPrice=tPrice;
+		_pizzaList =pizzaList;
+		}
+
+		public int OrdreNo
+		{  
+		get { return _orderNo; } 
+		set { _orderNo = value; }
+		}
+		public int Date
+		{
+		 get { return _date; }
+		 set { _date = value; }
+		}
+		public double TPrice
+		{
+		get { return _tPrice; }
+		set { _tPrice = value; }
+		}
+		public List<Pizza> PizzaList
+		{
+		get { return _pizzaList; }
+		set { _pizzaList =value; }
+		}
+
 	}
 }
--- ClassLibPizza/Model/OrdreListe.cs
+++ ClassLibPizza/Model/OrdreListe.cs
@@ -4,7 +4,8 @@ using System.Text;
 
 namespace ClassLibPizza.Model
 {
-	internal class OrdreListe
+	public class OrdreListe
 	{
+
 	}
 }
